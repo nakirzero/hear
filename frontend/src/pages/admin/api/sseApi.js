a10fbb12e25bce2c779a3c2fcf6676f377bea6a4
Lim Hyun Jin <jtothemoon@naver.com>
2024-11-10 18:04:03 +0900

    소스 원복(알림 문제)

--- frontend/src/pages/admin/api/sseApi.js
+++ frontend/src/pages/admin/api/sseApi.js
@@ -1,14 +1,13 @@
 export const initializeSSE = (setNotifications) => {
-  const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
-  const eventSource = new EventSource(`${backendUrl}/api/notification/stream`);
-
-  eventSource.onmessage = (event) => {
+    const eventSource = new EventSource('http://localhost:5000/api/notification/stream');
+    eventSource.onmessage = (event) => {
       console.log('New SSE event:', event.data);
       setNotifications((prevNotifications) => [
-          ...prevNotifications,
-          event.data,
+        ...prevNotifications,
+        event.data,
       ]);
+    };
+  
+    return eventSource; // 반환하여 필요 시 외부에서 연결 해제 가능
   };
-
-  return eventSource;
-};
+  
