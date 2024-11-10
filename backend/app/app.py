0da5682e6dca92efdcfa4657a4e562fbaba2aaf4
Lim Hyun Jin <jtothemoon@naver.com>
2024-11-10 17:09:04 +0900

    운영 환경 변경

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,7 +1,6 @@
-from flask import Flask
+from flask import Flask, Response
 from flask_cors import CORS
 from app.routes import register_blueprints
-from app.utils.sse_utils import get_sse_response
 
 app = Flask(__name__)
 
@@ -11,9 +10,13 @@ CORS(app)
 # Blueprint 등록
 register_blueprints(app)
 
-@app.route('/stream')
-def stream():
-    return get_sse_response()
+@app.after_request
+def add_custom_headers(response):
+    response.headers['Access-Control-Allow-Private-Network'] = 'true'  # 비공개 네트워크 허용
+    response.headers['Access-Control-Allow-Origin'] = 'https://h-ear.site'  # 필요에 따라 수정
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- frontend/src/pages/admin/api/sseApi.js
+++ frontend/src/pages/admin/api/sseApi.js
@@ -1,7 +1,7 @@
 export const initializeSSE = (setNotifications) => {
   const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
   const eventSource = new EventSource(`${backendUrl}/api/notification/stream`);
-  
+
   eventSource.onmessage = (event) => {
       console.log('New SSE event:', event.data);
       setNotifications((prevNotifications) => [
@@ -10,5 +10,5 @@ export const initializeSSE = (setNotifications) => {
       ]);
   };
 
-  return eventSource; // 반환하여 필요 시 외부에서 연결 해제 가능
+  return eventSource;
 };
