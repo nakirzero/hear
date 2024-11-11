05bd6c0a05340a6da50a172c3892f6201ae70865
Jtothemoon <jtothemoon@naver.com>
2024-11-12 00:28:50 +0900

    브레드크럼, 카피라이트(관리자) 수정

--- frontend/src/pages/admin/components/Copyright.js
+++ frontend/src/pages/admin/components/Copyright.js
@@ -6,8 +6,8 @@ export default function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://jtothemoon87.notion.site/10a95f91db4c8141986fe52b92c10807?v=fff95f91db4c81438251000cd300f383">
+        H-EAR
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
